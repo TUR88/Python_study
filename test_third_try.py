5a7df3b9d5e2c1686fb4a4315555536c7bfc74a0
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-03-22 15:46:56 +0300

    Add files via upload

---
+++ test_third_try.py
@@ -0,0 +1,19 @@
+import pytest
+import socket as s
+
+@pytest.fixture
+def socket(request):
+    _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+    def socket_teardown():
+        _socket.close()
+    request.addfinalizer(socket_teardown)
+    return _socket
+
+def test_server_data_check(socket):
+    sock = s.socket()
+    sock.connect(('localhost', 9090))
+    sock.send('Hello, my name is NOT Sergey'.encode())
+    data = sock.recv(1024)
+    assert data != ('Nice to meet you'.encode())
+    sock.close()
