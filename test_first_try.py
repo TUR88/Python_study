9192f76868f678d60a531d65ee1688af29ae7f52
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-03-27 10:19:30 +0300

    Update test_first_try.py

--- test_first_try.py
+++ test_first_try.py
@@ -1,18 +1,17 @@
 import pytest
-import socket as s
+import socket as s                # импортируем библиотеку socket, присваиваем переменной s
 
 
-@pytest.fixture
-def socket(request):
+@pytest.fixture                   # запускаем фикстуру pytest
+def socket(request):              # создаем функцию socket
     _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 
-    def socket_teardown():
+    def socket_teardown():        # создаем функцию socket_teardown
         _socket.close()
     request.addfinalizer(socket_teardown)
     return _socket
 
 
-def test_server_connect(socket):
-
+def test_server_connect(socket):   # проверяем наличие связи
     socket.connect(('localhost', 9090))
     assert socket
