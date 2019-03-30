4e0ac6a95cb29e46baa3fcfa2b92fedbbde24480
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-03-30 17:55:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,19 @@
-import socket #импортируем библиотеку socket
-
-sock = socket.socket() #создаем переменную sock ссылающуюся на параметр socket
-sock.bind(('', 9090)) #задаем номер порта и хост. В данной программе и клиент, и сервер находились на одной машине.
+import socket   # импортируем библиотеку
+sock = socket.socket()  # cоздаем переменную sock
+sock.bind(('', 9090))  # задаем номер порта и хост
 sock.listen(3)
-conn, addr = sock.accept() #соединяемся, присваиваимваем ip-адрес соединенного клиента и выводим на печать
+conn, addr = sock.accept()  # соединяемся, присваиваимваем ip-адрес
 
 print('connected:', addr)
 
 while True:
-    data = conn.recv(4096) #присваеиваем переменной полученные от клиента данные, указываем объем данных, ожидаем прием
+    data = conn.recv(4096)  # присваеиваем переменной полученные данные
     if not data:
         break
-    print(data) #выводим данные на печать
-    if data == ('Hello, my name is Sergey'.encode()): #если принятые данные равно одной величине высылаем один ответ
+    print(data)  # выводим данные на печать
+    if data == ('Hello, my name is Sergey'.encode()):
         conn.send('Nice to meet you!'.encode())
     else:
-        conn.send('Go away'.encode()) #если принятые данные не равны данной величине высылвем другой ответ
+        conn.send('Go away'.encode())  # высылаем другой ответ
 
-conn.close() #закрываем соединение
+conn.close()  # закрываем соединение
