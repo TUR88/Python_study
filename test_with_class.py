c5f9a0d84db43dd8a6b351010b16e86af12b10c9
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-01 11:53:11 +0300

    Update test_with_class.py

--- test_with_class.py
+++ test_with_class.py
@@ -2,28 +2,28 @@ import pytest
 import socket as s
 
 
-@pytest.fixture
-def socket(request):
-    _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-
-    def socket_teardown():
-        _socket.close()
-    request.addfinalizer(socket_teardown)
-    return _socket
-
-
-class Fte:
+class TestE:
     def __init__(self, message):
         self.message = message
+
+    @pytest.fixture
+    def socket(self, request):
+        _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+        def socket_teardown():
+            _socket.close()
+        request.addfinalizer(socket_teardown)
+        return _socket
 
     def test_server_data_check(self):
         sock = s.socket()
         sock.connect(('localhost', 9090))
-        sock.send('Hello, my name is Sergey'.encode())
+        sock.send('Hello, my name is '.encode())
         data = sock.recv(4096)
+        data = str(data, 'utf-8')
         assert data == self.message
         sock.close()
 
 
-f = Fte('Nice to meet you!')
+f = TestE('Go Away')
 f.test_server_data_check()
