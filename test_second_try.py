1fce9f38a7ba59954f8db2cb1c85e1db70cecd95
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-01 14:52:08 +0300

    Update test_second_try.py

--- test_second_try.py
+++ test_second_try.py
@@ -2,20 +2,34 @@ import pytest
 import socket as s
 
 
-@pytest.fixture
-def socket(request):
-    _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+class TestClass:
 
-    def socket_teardown():
-        _socket.close()
-    request.addfinalizer(socket_teardown)
-    return _socket
+    @pytest.fixture
+    def socket(request):
+        _socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 
+        def socket_teardown():
+            _socket.close()
 
-def test_server_data_check(socket):
-    sock = s.socket()
-    sock.connect(('localhost', 9090))
-    sock.send('Hello, my name is Sergey'.encode())
-    data = sock.recv(1024)
-    assert data == ('Nice to meet you!'.encode())
-    sock.close()
+        request.addfinalizer(socket_teardown)
+        return _socket
+
+    def test_server_connect(socket):  # проверяем наличие связи
+        socket.connect(('localhost', 9090))
+        assert socket
+
+    def test_server_data_check_true(self):
+        sock = s.socket()
+        sock.connect(('localhost', 9090))
+        sock.send('Hello, my name is Sergey'.encode())
+        data = sock.recv(1024)
+        assert data == ('Nice to meet you!'.encode())
+        sock.close()
+
+    def test_server_data_check_false(self):
+        sock = s.socket()
+        sock.connect(('localhost', 9090))
+        sock.send('Hello, I have NO name'.encode())
+        data = sock.recv(1024)
+        assert data == ('Go away'.encode())
+        sock.close()
