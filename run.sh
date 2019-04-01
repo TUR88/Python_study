e85c4381eda2cb89521e75d85f393b86458abd09
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-01 15:14:58 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,3 @@
 docker run -p 8000:80 sergio/tengine:2.3.0 &
 python3 /Users/sergey/PycharmProjects/untitled/Server.py &
 pytest /Users/sergey/PycharmProjects/untitled/test_with_class.py &
-python3 /Users/sergey/PycharmProjects/untitled/Server.py &
-pytest /Users/sergey/PycharmProjects/untitled/test_with_class.py &
