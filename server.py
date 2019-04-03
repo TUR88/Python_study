dc38c6e79870496720175c8f4faa9a46fcdcdf7c
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-03 13:45:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ while True:
     sock.bind(('', 9090))  # задаем номер порта и хост
     sock.listen(10)
     conn, addr = sock.accept()  # соединяемся, присваиваимваем ip-адрес
-
     print('connected:', addr)
     data = conn.recv(1024)  # присваеиваем переменной полученные данные
     print(data)  # выводим данные на печать
