403c9143906536ae3ff714814ce1a57a0d39e90d
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-03 18:08:26 +0300

    Delete site_upload.py

--- site_upload.py
+++
@@ -1,9 +0,0 @@
-import urllib3
-from bs4 import BeautifulSoup
-
-url = 'http://alterportal.ru'
-
-http = urllib3.PoolManager()
-response = http.request('GET', url)
-soup = BeautifulSoup(response.data, "html.parser")
-print(soup)
