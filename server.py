a1c71880c5870a013284ac2ac019da9ec1d739da
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-03 13:24:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,16 @@
-import socket   # импортируем библиотеку
-sock = socket.socket()  # cоздаем переменную sock
-sock.bind(('', 9090))  # задаем номер порта и хост
-sock.listen(3)
-conn, addr = sock.accept()  # соединяемся, присваиваимваем ip-адрес
+import socket  # импортируем библиотеку
 
-print('connected:', addr)
 
 while True:
-    data = conn.recv(4096)  # присваеиваем переменной полученные данные
-    if not data:
-        break
+    sock = socket.socket()  # cоздаем переменную sock
+    sock.bind(('', 9090))  # задаем номер порта и хост
+    sock.listen(10)
+    conn, addr = sock.accept()  # соединяемся, присваиваимваем ip-адрес
+
+    print('connected:', addr)
+    data = conn.recv(1024)  # присваеиваем переменной полученные данные
     print(data)  # выводим данные на печать
     if data == ('Hello, my name is Sergey'.encode()):
         conn.send('Nice to meet you!'.encode())
     else:
         conn.send('Go away'.encode())  # высылаем другой ответ
-
-conn.close()  # закрываем соединение
