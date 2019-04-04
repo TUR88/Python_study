d10e426043a0fcaf660ac3b6d58096e7acfdbe04
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-04 12:03:34 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run -p 8000:80 sergio/tengine:2.3.0
 python3 /Users/sergey/PycharmProjects/untitled/Server.py
-pytest -v -k "test_server_connect" /Users/sergey/PycharmProjects/untitled/test_second_try.py
-pytest -v -k "test_server_data_check_true" /Users/sergey/PycharmProjects/untitled/test_second_try.py
-pytest -v -k "test_server_data_check_false" /Users/sergey/PycharmProjects/untitled/test_second_try.py
+pytest -v /Users/sergey/PycharmProjects/untitled/test_second_try.py::TestClass1
+pytest -v /Users/sergey/PycharmProjects/untitled/test_second_try.py::TestClass2
+pytest -v /Users/sergey/PycharmProjects/untitled/test_second_try.py::TestClass3
