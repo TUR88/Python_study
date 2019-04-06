339df1f01d7b394a8cb67add5d7526c6dcb75462
TUR88 <48786500+TUR88@users.noreply.github.com>
2019-04-06 17:21:14 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -p 8000:80 sergio/tengine:2.3.0
-python3 /Users/sergey/PycharmProjects/untitled/Server.py
-pytest -v /Users/sergey/PycharmProjects/untitled/test_second_try.py::TestClass1
-pytest -v /Users/sergey/PycharmProjects/untitled/test_second_try.py::TestClass2
-pytest -v /Users/sergey/PycharmProjects/untitled/test_second_try.py::TestClass3
+docker run -p 8000:80 sergio/tengine:2.3.0 &
+python3 /Users/sergey/PycharmProjects/untitled/Server.py &
+pytest -v /Users/sergey/PycharmProjects/untitled/test_with_class.py::TestClass1 &
+pytest -v /Users/sergey/PycharmProjects/untitled/test_with_class.py::TestClass2 &
+pytest -v /Users/sergey/PycharmProjects/untitled/test_with_class.py::TestClass3 &
